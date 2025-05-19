7cb988935e637bed844c982427018d106ae7d01d
Nataliia <natashazaremba82.gmail.com>
2025-05-19 14:07:34 +0300

    addchanges

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,34 +1,62 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  overlayOpacity: 0.9,
-});
+// Ініціалізація Lightbox
+let lightbox;
 
 export function createGallery(images) {
   const galleryContainer = document.querySelector('.gallery');
+
   const galleryMarkup = images
     .map(
       image => `
-    <li class="gallery-item">
-      <a href="${image.largeImageURL}">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      </a>
-    </li>
-  `
+      <div class="photo-card">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <div class="info-item">
+            <b>Likes</b>
+            <span>${image.likes}</span>
+          </div>
+          <div class="info-item">
+            <b>Views</b>
+            <span>${image.views}</span>
+          </div>
+          <div class="info-item">
+            <b>Comments</b>
+            <span>${image.comments}</span>
+          </div>
+          <div class="info-item">
+            <b>Downloads</b>
+            <span>${image.downloads}</span>
+          </div>
+        </div>
+      </div>
+    `
     )
     .join('');
 
-  galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-  lightbox.refresh();
+  galleryContainer.innerHTML = galleryMarkup;
+
+  // Ініціалізація або оновлення lightbox
+  if (lightbox) {
+    lightbox.refresh();
+  } else {
+    lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
 }
 
+// Очищення галереї
 export function clearGallery() {
-  document.querySelector('.gallery').innerHTML = '';
+  const galleryContainer = document.querySelector('.gallery');
+  galleryContainer.innerHTML = '';
 }
 
+// Відображення/приховування лоадера
 export function showLoader() {
   document.querySelector('.loader').classList.remove('is-hidden');
 }
